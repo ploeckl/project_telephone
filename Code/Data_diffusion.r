b0ce8d9eb16b73e67929e430b52781888d47bc71
ploeckl <ploeckl@outlook.com>
2020-11-27 15:58:25 +1030

    Create new code file for creating plots and start shifting code over

--- Code/Data_diffusion.r
+++ Code/Data_diffusion.r
@@ -254,18 +254,4 @@ write.dta(as.data.frame(RegionWeight), "C:\\Box\\Research\\Telephone\\project_te
 
 
 
-#############################
-###########################
-#Plot presentation
 
-Cumulative<-mat.or.vec(max(Towns$InstallMonth),2)
-Cumulative[,1]<-seq(1,max(Towns$InstallMonth),1)
-for (i in 1: dim(Cumulative)[1]){
-Cumulative[i,2]<-sum(Towns$InstallMonth<=i)
-}
-plot(Cumulative, type="l", ylab="Number of Local Exchanges", xlab="")
-
-
-Penetration<-as.matrix(Towns[,c("Penetration1896","Penetration1900","Penetration1905")])
-Penetration[Penetration==0]<-NA
-matplot(c(1896,1900,1905),t(Penetration),col="BLACK",ylab="Phone Lines per Capita", xlab="Year",type="p",pch=19, lty=3)
